b98be2f6bc798130ac245e9578ddfb228bec9d0f
gabriel <gabriel.pr07@hotmail.com>
2023-05-11 17:20:13 -0300

    feat: add recipeupdate modal

--- frontend/src/pages/RecipeDetails/[slug].tsx
+++ frontend/src/pages/RecipeDetails/[slug].tsx
@@ -6,36 +6,106 @@ import { RecipeData } from '@/components/RecipeList'
 import EditingIcon from '../../../public/editing.svg'
 import DeleteIcon from '../../../public/close2.svg'
 import Link from 'next/link'
+import { api } from '../../../services/api'
+import { GetServerSideProps } from 'next'
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher'
+import Dialog from '@material-ui/core/Dialog'
+import { FormEvent, useState } from 'react'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogActions from '@material-ui/core/DialogActions'
+import Button from '@material-ui/core/Button'
+import { DialogContentText } from '@material-ui/core'
+import { toast } from 'react-toastify'
+import Router from 'next/router'
+import { useForm } from 'react-hook-form'
 
 type RecipeDetailsProps = {
+  slug: string
   recipe: RecipeData
 }
 
-export default function RecipeDetails({ recipe }: RecipeDetailsProps) {
+export default function RecipeDetails({ slug, recipe }: RecipeDetailsProps) {
+  const [ name, setName ] = useState(recipe.name)
+  const [ weight, setWeight ] = useState<number>(recipe.weight)
+  const [ assemblyIngradients, setAssemblyIngradients ] = useState(recipe.assemblyIngradients)
+  const [ operatingInstructions, setOperatingInstructions ] = useState(recipe.operatingInstructions)
+  const [ entryTemperature, setEntryTemperature ] = useState(recipe.entryTemperature)
+  const [open, setOpen] = useState(false)
+  const [openEdit, setOpenEdit] = useState(false)
+  const {handleSubmit, register} = useForm()
+  const [recipeData, setRecipeData] = useState(recipe)
+
+  async function handleDelete(id: string) {
+    try {
+      const res = await api.delete(`recipe/${id}`)
+
+      if(res.status === 204) {
+        toast.success('Recipe deleted successfuly!')
+        Router.push('/');;
+      } else if (res.status===500){
+        toast.error('Erro no servidor!')
+      }
+    }catch (err) {
+      toast.error(`${err}`);
+    }
+  }
+
+  async function handleUpdateRecipe(data:any) {
+
+    try {
+      const res = await api.patch(`recipe/${slug}`, data)
+      console.log('',res)
+      if(res.status === 204) {
+        toast.success('Successfully updated recipe')
+      } else if (res.status===500){
+        toast.error('Server error')
+      }
+    }catch(err) {
+      toast.error(`${err}`);
+    }
+  }
+
+  const handleCloseEdit = () => {
+    setOpenEdit(false);
+  };
+
+  const handleClickOpenEdit = () => {
+    setOpenEdit(true);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={styles.recipeDetailsContainer}>
       <SideBar/>
 
-
-      <div className={styles.asideContent}>
+      { recipe?.name ? (
+        <div className={styles.asideContent}>
         <div className={styles.recipeHeader}>
           <div className={styles.titles}>
             <Link href='/'>
               <Image alt='back' src={BackLogo}/>
             </Link>
             <div>
-              <h2>recipe</h2>
+              <h2>{recipe.name}</h2>
               <span> FIT Express</span>
             </div>
           </div>   
 
           <div className={styles.actions}>
-            <div className={styles.edit}>
+            <div className={styles.edit} onClick={() => handleClickOpenEdit()}>
               <Image alt='edit' src={EditingIcon}/>
               <button>Edit</button>
             </div>
             
-            <div className={styles.delete}>
+            <div className={styles.delete} onClick={() => handleClickOpen()}>
               <Image alt='delete' src={DeleteIcon}/>
               <button>Delete</button>
             </div>
@@ -50,27 +120,145 @@ export default function RecipeDetails({ recipe }: RecipeDetailsProps) {
             <div className={styles.gramsAndTemp}>
               <div className={styles.weight}>
                 <div> Weight(grams) </div> 
-                <span> 200 </span>
+                <span> {recipe.weight} </span>
               </div>
               <div className={styles.temp}>
                 <div>Entry temp.(ºC) </div>
-                <span>23ºC</span>
+                <span>{recipe.entryTemperature}</span>
               </div>
             </div>
 
             <div className={styles.assemblyIngradients}>
               <div className={styles.assemblyTitle}> Assembly Ingradients: </div>
-              <p>x</p>
+              <p>{recipe.assemblyIngradients}</p>
             </div>
             <div className={styles.operatingInstructions}>
               <div className={styles.operatingTitle}> Operating Instructions: </div>
-              <p>oi</p>
+              <p>{recipe.operatingInstructions}</p>
             </div>
           </div>
         </div>
+
+        <Dialog open={openEdit} onClose={handleCloseEdit} className={styles.dialogContainer}>
+          <div className={styles.formHeader}>
+            <DialogTitle>
+              <div className={styles.cr}>
+                Update Recipe
+              </div> 
+            </DialogTitle>
+          </div>
+          
+          <DialogContent>
+            <form className={styles.formContainer} onSubmit={handleSubmit(handleUpdateRecipe)}>
+              <div className={styles.topForm}>
+                <div className={styles.name}>
+                  <label htmlFor="name">Name</label>
+                  <input
+                   {...register('name')}
+                    type='text'
+                    id='name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+              
+                <div className={styles.weight}>
+                  <label htmlFor="">Weight</label>
+                  <input
+                   {...register('weight')}
+                    type='number'
+                    id='weight'
+                    value={weight}
+                    onChange={(e) => setWeight(Number(e.target.value))}
+                  />
+                </div>
+
+              </div>
+
+              <div className={styles.middleForm}>
+                <div className={styles.assemblyIng}>
+                  <label htmlFor="">Assembly Ing</label>
+                  <textarea
+                   {...register('assemblyIngradients')}
+                    maxLength={300}
+                    id="assemblyIngradients"
+                    value={assemblyIngradients}
+                    onChange={(e) => setAssemblyIngradients(e.target.value)}
+                  />
+                </div>
+
+                <div className={styles.operatinIns}>
+                  <label htmlFor="">Op. Instructions</label>
+                  <textarea 
+                   {...register('operatingInstructions')}
+                    maxLength={300}
+                    id='operatingInstructions'
+                    value={operatingInstructions}
+                    onChange={(e) => setOperatingInstructions(e.target.value)}
+                  />
+                </div>
+
+                <div className={styles.entryTemperature}>
+                  <label htmlFor="">Entry temp. (ºC)</label>
+                  <input
+                   {...register('entryTemperature')}
+                    type='string'
+                    id='entryTemperature'
+                    value={entryTemperature}
+                    onChange={(e) => setEntryTemperature(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <DialogActions>
+                <Button onClick={handleCloseEdit} color="secondary">
+                  Cancel
+                </Button>
+                <Button type='submit' color='primary'>
+                  Save
+                </Button>
+              </DialogActions>
+            </form>
+          </DialogContent>
+        </Dialog>
+
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Deletar carro</DialogTitle>
+          <DialogContent>
+              <DialogContentText>
+                Tem certeza que quer deletar?
+              </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={() => {
+                  handleDelete(recipe.id);
+                  handleClose();
+              }} color="secondary">
+                  Deletar
+              </Button>
+          </DialogActions>
+        </Dialog>
       </div>
+      ): ''}
+      
     </div>
   )
 }
 
 
+export const getServerSideProps: GetServerSideProps = async ({ params }: Params) => {
+  const { slug } = params
+
+  const recipeData = await api.get(`recipe/${slug}`)
+  const recipe = recipeData.data 
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return {
+    props: {
+      slug,
+      recipe
+    }
+  }
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,10 +2,91 @@ import { GetServerSideProps } from 'next'
 import { api } from '../../services/api'
 import styles from './styles.module.scss'
 import SideBar from '@/components/SideBar'
-import RecipeList, { RecipeListProps } from '@/components/RecipeList'
+import RecipeList, { RecipeData, RecipeListProps } from '@/components/RecipeList'
+import { FiPlus } from 'react-icons/fi'
+import { 
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent, 
+  DialogTitle, 
+  TextField 
+} from '@material-ui/core'
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
 
 
 export default function Landing({ recipes } : RecipeListProps) {
+  const [ name, setName ] = useState('')
+  const [ weight, setWeight ] = useState<number>()
+  const [ assemblyIngradients, setAssemblyIngradients ] = useState('')
+  const [ operatingInstructions, setOperatingInstructions ] = useState('')
+  const [ entryTemperature, setEntryTemperature ] = useState('')
+  const [ recipesWrapper, setRecipesWrapper] = useState<RecipeData[]>(recipes)
+  const { register,handleSubmit, reset} = useForm()
+  const [ file, setFile ] = useState<File>()
+  const [ image, setImage ] = useState()
+  const [ open, setOpen ] = useState(false)
+  const [ previewImage, setPreviewImage ] = useState<string>()
+
+  useEffect(() => {
+    async function fetchRecipes() {
+      const response = await api.get('recipes')
+      setRecipesWrapper(response.data)
+    }
+    
+    fetchRecipes()
+  }, [recipesWrapper])
+  
+
+  const handleSelectedImage = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedImage = event.target.files?.[0]
+
+    if (!selectedImage) {
+      return;
+    }
+
+    setFile(selectedImage)
+
+    const selectedImagePreview = URL.createObjectURL(selectedImage)
+
+    setPreviewImage(selectedImagePreview)
+  }
+
+  const handleSaveRecipe = async (event:FormEvent) => {
+    event.preventDefault()
+
+    const data = new FormData()
+    data.append('name',name)
+    data.append('weight', String(weight))
+    data.append('assemblyIngradients', assemblyIngradients)
+    data.append('operatingInstructions', operatingInstructions)
+    data.append('entryTemperature', entryTemperature)
+    if (file instanceof Blob) {
+      data.append('file', file);
+    } else {
+      console.error('O arquivo não é válido');
+    }
+    
+    const res = await api.post('recipe',data)
+
+    if(res.status === 204) {
+      toast.success('Recipe successfully registered!')
+      handleClose()
+    }else {
+      toast.error('Required field not filled in!')
+    }
+    
+  }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={styles.landingContainer}>
@@ -14,7 +95,12 @@ export default function Landing({ recipes } : RecipeListProps) {
         <div className={styles.sideHeader}>
           <div className={styles.top}>
             <h2>CookBook</h2>
-            <button className={styles.createRecipe}>+ Create recipe</button>
+            <button 
+              className={styles.createRecipe}
+              onClick={() => handleClickOpen()}
+            >
+              + Create recipe
+            </button>
           </div>
 
           <input 
@@ -26,7 +112,100 @@ export default function Landing({ recipes } : RecipeListProps) {
           />
         </div>
         
-        <RecipeList recipes={recipes}/>
+        <RecipeList recipes={recipesWrapper}/>
+
+        <Dialog open={open} onClose={handleClose} className={styles.dialogContainer}>
+          <div className={styles.formHeader}>
+            <DialogTitle>
+              <div className={styles.cr}>
+                Create Recipe
+              </div> 
+            </DialogTitle>
+          </div>
+          
+          <DialogContent>
+            <form className={styles.formContainer} onSubmit={handleSaveRecipe}>
+              <div className={styles.topForm}>
+                <div className={styles.name}>
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type='text'
+                    id='name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+              
+                <div className={styles.weight}>
+                  <label htmlFor="">Weight</label>
+                  <input
+                    type='number'
+                    id='weight'
+                    value={weight}
+                    onChange={(e) => setWeight(Number(e.target.value))}
+                  />
+                </div>
+
+              </div>
+
+              <div className={styles.middleForm}>
+                <div className={styles.assemblyIng}>
+                  <label htmlFor="">Assembly Ing</label>
+                  <textarea
+                    maxLength={300}
+                    id="assemblyIngradients"
+                    value={assemblyIngradients}
+                    onChange={(e) => setAssemblyIngradients(e.target.value)}
+                  />
+                </div>
+
+                <div className={styles.operatinIns}>
+                  <label htmlFor="">Op. Instructions</label>
+                  <textarea 
+                    maxLength={300}
+                    id='operatingInstructions'
+                    value={operatingInstructions}
+                    onChange={(e) => setOperatingInstructions(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className={styles.upload}>
+                {previewImage ? (
+                  <img src={previewImage} alt='img'/>
+                ): (<div></div>)}
+                
+                <label htmlFor="uprecipe" className={styles.newImage}>      
+                  <FiPlus size={24} color='#6F48C9'/>
+                </label>
+                <input
+                  onChange={handleSelectedImage} 
+                  type="file" 
+                  name="uprecipe" 
+                  id="uprecipe" 
+                />
+
+                <div className={styles.entryTemperature}>
+                  <label htmlFor="">Entry temp. (ºC)</label>
+                  <input
+                    type='string'
+                    id='entryTemperature'
+                    value={entryTemperature}
+                    onChange={(e) => setEntryTemperature(e.target.value)}
+                  />
+                </div>
+              </div>
+              <DialogActions>
+                <Button onClick={handleClose} color="secondary">
+                  Cancel
+                </Button>
+                <Button type='submit' color='primary'>
+                  Save
+                </Button>
+              </DialogActions>
+            </form>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   )
